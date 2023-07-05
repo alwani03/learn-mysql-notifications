3d840b1f62f84a93165fe0c1817dc707284e95d3
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-07-05 14:14:06 +0700

    User

--- mysql-code/create_database.sql
+++ mysql-code/create_database.sql
@@ -18,3 +18,38 @@ INSERT INTO user(id, name)
 VALUES ('nurul', 'Nurul Karomah');
 
 SELECT * FROM USER;
+
+
+/*  Notification */
+CREATE TABLE notification
+(
+    id        INT          NOT NULL AUTO_INCREMENT,
+    title     VARCHAR(255) NOT NULL,
+    detail    TEXT         NOT NULL,
+    create_at TIMESTAMP    NOT NULL,
+    user_id   VARCHAR(100),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+SHOW TABLES;
+
+ALTER TABLE notification
+    ADD CONSTRAINT fk_notification_user
+        FOREIGN KEY (user_id) REFERENCES user (id);
+
+DESC notification;
+
+/* Insert Table Notification */
+
+INSERT INTO notification(title, detail, create_at, user_id)
+VALUES ('Contoh Pesanan', 'Detail Pesanan', CURRENT_TIMESTAMP(), 'alwani');
+INSERT INTO notification(title, detail, create_at, user_id)
+VALUES ('Contoh Promo', 'Detail Promo', CURRENT_TIMESTAMP(), null);
+INSERT INTO notification(title, detail, create_at, user_id)
+VALUES ('Contoh Pembayaran', 'Detail Pembayaran', CURRENT_TIMESTAMP(), 'nurul');
+
+SELECT *
+FROM notification;
+
+SELECT * FROM notification
+WHERE  (user_id = 'alwani' OR user_id IS NULL) ORDER BY create_at DESC;
