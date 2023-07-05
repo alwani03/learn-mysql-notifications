ce2a3f41e407ea02e2e5cfe8103fa2108de02f52
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-07-05 14:49:29 +0700

    Category

--- mysql-code/create_database.sql
+++ mysql-code/create_database.sql
@@ -53,3 +53,80 @@ FROM notification;
 
 SELECT * FROM notification
 WHERE  (user_id = 'alwani' OR user_id IS NULL) ORDER BY create_at DESC;
+
+SELECT * FROM notification
+WHERE  (user_id = 'nurul' OR user_id IS NULL) ORDER BY create_at DESC;
+
+
+/*  Category */
+CREATE TABLE category
+(
+    id   VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+
+SHOW TABLES;
+
+ALTER TABLE notification
+    ADD COLUMN category_id VARCHAR(100);
+
+DESCRIBE notification;
+
+ALTER TABLE notification
+    ADD CONSTRAINT fk_notification_category
+        FOREIGN KEY (category_id) REFERENCES category (id);
+
+DESC notification;
+
+SELECT *
+FROM notification;
+
+
+INSERT INTO category(id, name)
+VALUES ('INFO', 'Info');
+INSERT INTO category(id, name)
+VALUES ('PROMO', 'Promo');
+
+SELECT *
+FROM category;
+
+UPDATE notification
+SET category_id = 'INFO'
+WHERE id = 1;
+
+UPDATE notification
+SET category_id = 'PROMO'
+WHERE id = 2;
+
+UPDATE notification
+SET category_id = 'INFO'
+WHERE id = 3;
+
+SELECT *
+FROM notification;
+
+/* Menampilkan per category */
+SELECT *
+FROM notification n
+         JOIN category c ON (n.category_id = c.id)
+WHERE (n.user_id = 'alwani' OR n.user_id IS NULL)
+ORDER BY n.create_at DESC;
+
+SELECT *
+FROM notification n
+         JOIN category c ON (n.category_id = c.id)
+WHERE (n.user_id = 'nurul' OR n.user_id IS NULL)
+ORDER BY n.create_at DESC;
+
+
+SELECT *
+FROM notification n
+         JOIN category c ON (n.category_id = c.id)
+WHERE (n.user_id = 'alwani' OR n.user_id IS NULL)
+  AND c.id = 'INFO'
+ORDER BY n.create_at DESC;
+
+
+        
