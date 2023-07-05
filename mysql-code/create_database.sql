8d9d1d5f6768a4362a5cb1954d504721ec7271f1
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-07-05 09:11:08 +0700

    Table User

--- mysql-code/create_database.sql
+++ mysql-code/create_database.sql
@@ -1,2 +1,20 @@
 CREATE DATABASE belajar_mysql_notification
 use belajar_mysql_notification
+
+/* User */
+CREATE TABLE user
+(
+    id   VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+SHOW TABLES;
+
+/* Insert Table User */
+INSERT INTO user(id, name)
+VALUES ('alwani', 'achmad alwani');
+INSERT INTO user(id, name)
+VALUES ('nurul', 'Nurul Karomah');
+
+SELECT * FROM USER;
